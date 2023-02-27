8153e7291a30f60bf1e3111422d11342038cca41
Eric <eric2umeh@yahoo.com>
2023-02-27 14:29:24 +0100

    Update webpack.config.js for css rules

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,16 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
